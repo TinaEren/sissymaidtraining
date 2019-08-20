85b49ec0d4b5a021d5b8b55c071c458b0f49170b
TinaEren <2016unknow@gmail.com>
2019-08-20 15:52:19 +0200

    ajout gui.py

---
+++ gui.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  6 18:41:37 2019
+
+@author: survi
+"""
+
+from tkinter import *
+  
+from tkinter import ttk
+  
+window = Tk()
+style = ttk.Style()
+style.configure("TNotebook", background='DeepPink', foreground='white') 
+  
+window.title("Sissy Maid Training")
+window.geometry("800x550")  
+tab_control = ttk.Notebook(window, padding=(10, 5, 10, 5), style="TNotebook")
+
+
+
+
+tab1 = ttk.Frame(tab_control)
+  
+tab2 = ttk.Frame(tab_control)
+  
+tab_control.add(tab1, text='First')
+  
+tab_control.add(tab2, text='Second')
+  
+lbl1 = Label(tab1, text= 'label1')
+  
+lbl1.grid(column=0, row=0)
+  
+lbl2 = Label(tab2, text= 'label2')
+  
+lbl2.grid(column=0, row=0)
+  
+tab_control.pack(expand=1, fill='both')
+  
+window.mainloop()
+
