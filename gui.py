ca19fc40757ab3ecbabb58028248d491c731c05a
TinaEren <2016unknow@gmail.com>
2019-08-29 12:36:02 +0200

    add 4button

--- gui.py
+++ gui.py
@@ -25,12 +25,16 @@ tab1 = ttk.Frame(tab_control)
 tab2 = ttk.Frame(tab_control)
 
 tab3 = ttk.Frame(tab_control)
+
+tab4 = ttk.Frame(tab_control)
   
 tab_control.add(tab1, text='First')
   
 tab_control.add(tab2, text='Second')
 
 tab_control.add(tab3, text='Third')
+
+tab_control.add(tab4, text='Four')
 
   
 lbl1 = Label(tab1, text= 'label1')
@@ -44,6 +48,10 @@ lbl2.grid(column=0, row=0)
 lbl3 = Label(tab3, text= 'label3')
   
 lbl3.grid(column=0, row=0)
+
+lbl4 = Label(tab4, text= 'label4')
+
+lbl4.grid(column=0, row=0)
 
   
 tab_control.pack(expand=1, fill='both')
